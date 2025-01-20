f0042cfd3504455524edb1f82f1607b7c47bbd83
working2003 <taskworkhub@gmail.com>
2025-01-20 14:41:30 +0530

    Update authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,8 +3,12 @@ const router = express.Router();
 const {sendOTP,verifyOTPAndLogin} = require("../controllers/authController")
 //const {registerUser} = require("../controllers/userController")
 
-router.route("/").post(sendOTP);
+router.route("/")
+  .get((req, res) => {
+    res.status(200).json({ message: "Login endpoint is working. Please make a POST request to send OTP." });
+  })
+  .post(sendOTP);
+  
 router.route("/verify").post(verifyOTPAndLogin);
- 
 
 module.exports = router;
